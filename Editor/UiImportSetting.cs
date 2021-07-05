37060e2486ce61bb69f614aa5d8846e7b4ea41fa
琴卓 <1043665802@qq.com>
2021-07-05 19:49:10 +0800

    1

--- Editor/UiImportSetting.cs
+++ Editor/UiImportSetting.cs
@@ -368,9 +368,9 @@ namespace QTool.Psd2Ui
             }
             var instancePrefab = PrefabUtility.InstantiatePrefab(prefab, tempUi.parent) as GameObject;
             var ui = instancePrefab.GetComponent<RectTransform>();
+            instancePrefab.transform.SetSiblingIndex(tempUi.GetSiblingIndex());
             ui.anchorMin = Vector2.one * 0.5f;
             ui.anchorMax = Vector2.one * 0.5f;
-            instancePrefab.transform.SetSiblingIndex(tempUi.GetSiblingIndex());
             instancePrefab.transform.ChangeTo(tempUi);
            // var size = ui.sizeDelta;
            //  = tempUi.sizeDelta;
@@ -430,10 +430,10 @@ namespace QTool.Psd2Ui
                 }
                 for (int i = 0; i < oldUi.childCount && i < newUI.childCount; i++)
                 {
-                    if (!UnityEditor.PrefabUtility.IsAnyPrefabInstanceRoot(oldUi.GetChild(i).gameObject))
-                    {
+                    //if (!UnityEditor.PrefabUtility.IsAnyPrefabInstanceRoot(oldUi.GetChild(i).gameObject))
+                    //{
                         oldUi.GetChild(i).ChangeTo(newUI.GetChild(i));
-                    }
+                   // }
                  
                 }
             }
